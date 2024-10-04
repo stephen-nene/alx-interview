3c23e45fa59bc0479273dbab35658a223b331df2
steve nene <stevekid705@gmail.com>
2024-10-04 11:50:24 +0300

    unfractored code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,14 @@
-from functools import reduce
-
 def pascal_triangle(n):
     if n <= 0:
         return []
 
-    def next_row(prev):
-        return [1] + [prev[i - 1] + prev[i] for i in range(1, len(prev))] + [1]
+    triangle = [[1]]
     
-    return reduce(lambda acc, _: acc + [next_row(acc[-1])], range(n - 1), [[1]])
-
-# Usage example:
-# print(pascal_triangle(5))
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, len(triangle[-1])):
+            row.append(triangle[-1][j - 1] + triangle[-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle

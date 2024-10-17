25eb12531195da49a4826a27de761a49d417b43b
steve nene <stevekid705@gmail.com>
2024-10-17 16:25:09 +0300

    lockbox done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    # Initialize a set to track boxes that can be opened
+    opened_boxes = {0}  # The first box (0) is already unlocked
+    keys = [0]  # Start with the keys from the first box
+
+    while keys:
+        current_key = keys.pop()  # Get a key
+        for key in boxes[current_key]:  # Iterate through the keys in the current box
+            if key not in opened_boxes:  # If this box hasn't been opened yet
+                opened_boxes.add(key)  # Mark it as opened
+                keys.append(key)  # Add the key to the list for further unlocking
+
+    # Check if all boxes can be opened
+    return len(opened_boxes) == len(boxes)
